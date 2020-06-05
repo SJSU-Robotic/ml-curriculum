25aa1b7d049227032f869f8fa1948cda6ede90e2
Nelson <ntwong0@gmail.com>
2020-06-05 00:36:22 -0700

    implemented compute_distances_two_loops

--- assignments/colab/2020/module1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignments/colab/2020/module1/cs231n/classifiers/k_nearest_neighbor.py
@@ -77,7 +77,7 @@ class KNearestNeighbor(object):
                 #####################################################################
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-                pass
+                dists[i, j] = np.sqrt(np.sum((X[i] - self.X_train[j])**2))
 
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
